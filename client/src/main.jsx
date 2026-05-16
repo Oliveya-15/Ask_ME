c17edf6624756c233a8fbe42962d38cbe29c8030
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2026-05-16 14:26:46 +0530

    Preparing for Deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,11 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/react'
+
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  </ClerkProvider>
+  <ClerkProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  </ClerkProvider>
 )
